a196e1b1d3468d2d01d692c1bebe33252dc8ac93
ryancdavey1 <ryancdavey1@gmail.com>
2020-11-20 10:19:20 -0700

    Done.

--- index.js
+++ index.js
@@ -1,33 +1,22 @@
-// Add your code here
-function submitData(name, email){
-  let formData = {
-    name: name,
-    email: email
-  };
-   
-  let configObj = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json"
-    },
-    body: JSON.stringify(formData)
-  };
-   
-  fetch("http://localhost:3000/users", configObj)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(object) {
-      console.log(object);
-      let body = document.querySelector('body');
-      let p = document.createElement('p'); 
-      p.innerText = object.id;   
-    })
-    .catch(function(error) {
-      let body = document.querySelector('body');
-      let p = document.createElement('p');
-      p.innerText = error.message;
-      console.log(error.message);
-    });
+function submitData( name, email ) {
+  return fetch( 'http://localhost:3000/users', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify( {
+        name,
+        email
+      } )
+    } )
+    .then( function ( response ) {
+      return response.json()
+    } )
+    .then( function ( object ) {
+      document.body.innerHTML = object[ "id" ]
+    } )
+    .catch( function ( error ) {
+      document.body.innerHTML = error.message
+    } )
 }
